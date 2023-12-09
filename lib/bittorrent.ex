a14ece9e16770363e8c94083599787763ad71dd8
Christopher N. KATOYI KABA <me@christopher2k.dev>
2023-12-09 16:58:30 -0500

    Stage 3

--- lib/bittorrent.ex
+++ lib/bittorrent.ex
@@ -2,7 +2,7 @@ defmodule Bittorrent.CLI do
   def main(argv) do
     case argv do
       ["decode" | [encoded_str | _]] ->
-        decoded_str = Bencode.decode(encoded_str)
+        {decoded_str, _} = Bencode.decode(encoded_str)
         IO.puts(Jason.encode!(decoded_str))
 
       [command | _] ->
@@ -17,24 +17,49 @@ defmodule Bittorrent.CLI do
 end
 
 defmodule Bencode do
+  def decode(<<"l"::binary, rest::binary>>) do
+    decode_list(rest)
+  end
+
   def decode(<<"i"::binary, rest::binary>>) do
-    <<mb_number::binary-size(rest |> byte_size |> Kernel.-(1)), "e"::binary>> = rest
-    {number, _} = Integer.parse(mb_number)
-    number
+    binary_data = :binary.bin_to_list(rest)
+    {head, tail} = binary_data |> Enum.split_while(fn char -> char != ?e end)
+
+    {decoded, _} = head |> List.to_string() |> Integer.parse()
+
+    rest =
+      tail |> Enum.slice(1..-1) |> List.to_string()
+
+    {decoded, rest}
   end
 
   def decode(encoded_value) when is_binary(encoded_value) do
     binary_data = :binary.bin_to_list(encoded_value)
+    {head, tail} = binary_data |> Enum.split_while(fn char -> char != ?: end)
 
-    case Enum.find_index(binary_data, fn char -> char == 58 end) do
-      nil ->
-        IO.puts("The ':' character is not found in the binary")
+    {size, _} = head |> List.to_string() |> Integer.parse()
 
-      index ->
-        rest = Enum.slice(binary_data, (index + 1)..-1)
-        List.to_string(rest)
-    end
+    decoded =
+      tail
+      |> Enum.slice(1..size)
+      |> List.to_string()
+
+    rest =
+      tail |> Enum.slice((size + 1)..length(tail)) |> List.to_string()
+
+    {decoded, rest}
   end
 
   def decode(_), do: "Invalid encoded value: not binary"
+
+  # DECODE COLLECTIONS
+  defp decode_list(encoded_items, result \\ [])
+
+  defp decode_list(<<"e"::binary, rest::binary>>, result),
+    do: {result |> Enum.reverse(), rest}
+
+  defp decode_list(encoded_items, result) do
+    {decoded_item, rest} = decode(encoded_items)
+    decode_list(rest, [decoded_item | result])
+  end
 end
